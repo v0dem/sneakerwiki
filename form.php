48894f3bf0dca05777c867dd73e4f1215dce0eb7
Vadim <vadimych610@gmail.com>
2018-07-17 21:12:38 +0300

    Sneaker
    Test

---
+++ form.php
@@ -0,0 +1,10 @@
+<form name="form1" method="post" action="test.php"> (начало формы)
+<p> Имя: <input type="text" name="sirname"> (поле имени)</p>
+<p> Ваш Email: <input  type="text" name="email"></p
+<p> Сообщение 
+<textarea name="message"></textarea> (поле сообщения)
+</p>
+<p>
+<input type="submit" name="send" value="Отправить"> (кнопка отправки)
+</p>
+</form> (конец формы) 
